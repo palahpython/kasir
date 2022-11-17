2df9e0402f4f0a8a505777e0f5c23ab5ba4e8286
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-17 14:53:11 +0700

    Update logi.py

--- logi.py
+++ logi.py
@@ -2,8 +2,10 @@ import streamlit as st
 st.write("""
 # SELAMAT DATANG DI KASIR SENYUM !
 """)
-st.text_input("Nama : ")
+st.text_input("Nama lengkap : ")
+st.text_input("Bank : ")
 
+st.selectbox()
 
 st.balloons()
 

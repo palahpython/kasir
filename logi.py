e5de0bcf0eaf684f3b3a93e60a3ca73d7fd29ce5
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-18 17:26:06 +0700

    Update logi.py

--- logi.py
+++ logi.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.write("""
 # Alpalah
 """)
-st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(https://assets-global.website-files.com/616d24eebbeca61bd01d9788/616d24eebbeca6885c1d9ac8_51024.jpg, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")

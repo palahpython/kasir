0bb90f9be1bc237c1ebdf3cec46897aaf35c9603
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 10:25:52 +0700

    Update daftar.py

--- daftar.py
+++ daftar.py
@@ -6,6 +6,8 @@ DAFTAR
 nama = st.text_input('Nama Lengkap : ')
 number = st.text_input('Nomor Hp. :')
 password = st.text_input('Kata Sandi :')
+daftar = st.button('KIRIM')
+
 
 st.write('Nama Lengkap : ',nama)
 st.write('Nomor Hp  : ',number)

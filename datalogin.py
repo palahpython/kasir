c437b2f0bf32eee69aaade670c3fc416d31ca9ca
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 11:16:57 +0700

    Update datalogin.py

--- datalogin.py
+++ datalogin.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-if nomor == '123' and password == '111':
-  st.write('Nomor & Password yang anda masukan benar!')
-
- else:
-  st.write('Nomor & Password yang anda masukan salah!')
+st.write("""
+# DATA LOGIN
+""")

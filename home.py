49d74ce84e2ac38998c1c01b87d58059a075142d
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 11:32:04 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,23 +4,8 @@ st.write("""
 # HOME
 """)
 
-def get_user_name():
-    return 'John'
-
-# ------------------------------------------------
-# Want people to see this part of the code...
-
-def get_punctuation():
-    return '!!!'
-
-greeting = "Hi there, "
-user_name = get_user_name()
-punctuation = get_punctuation()
-
-st.write(greeting, user_name, punctuation)
-
-# ...up to here
-# ------------------------------------------------
-
-foo = 'bar'
-st.write('Done!')
+dataframe = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+})
+st.experimental_show(dataframe)

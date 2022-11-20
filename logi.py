d838bd5bc0a0bb0c496feac382caef784b9b4093
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 09:30:38 +0700

    Update logi.py

--- logi.py
+++ logi.py
@@ -3,26 +3,7 @@ import streamlit as st
 st.image('kasir.jpg')
 st.write("""
 
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Halaman Login</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-   
-    <body>
-        <div class="container">
-          <h1>Login</h1>
-            <form>
-                <label>Username</label><br>
-                <input type="text"><br>
-                <label>Password</label><br>
-                <input type="password"><br>
-                <button>Log in</button>
-            </form>
-        </div>     
-    </body>
-</html>
+
 
 """)
 

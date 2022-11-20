05d87faf8ba36cb628f983637ef0f6ad9a76a795
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 11:30:37 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,3 +4,23 @@ st.write("""
 # HOME
 """)
 
+def get_user_name():
+    return 'John'
+
+# ------------------------------------------------
+# Want people to see this part of the code...
+
+def get_punctuation():
+    return '!!!'
+
+greeting = "Hi there, "
+user_name = get_user_name()
+punctuation = get_punctuation()
+
+st.write(greeting, user_name, punctuation)
+
+# ...up to here
+# ------------------------------------------------
+
+foo = 'bar'
+st.write('Done!')

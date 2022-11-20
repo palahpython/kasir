30d68026e286f0a4416cad421a3ec4213b781c59
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 10:01:04 +0700

    Update daftar.py

--- daftar.py
+++ daftar.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.write("""
 DAFTAR
 """)
+nama = st.text-input("Nama Lengkap :")
+umur = st.date_input()
+number = st.number_input("Nomor Hp. :")
+password = st.password_input("Kata Sandi :")

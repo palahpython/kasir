27b89d4f73f6ff7090088e3321f0ac3a500e4eb3
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-20 10:17:34 +0700

    Update daftar.py

--- daftar.py
+++ daftar.py
@@ -8,7 +8,6 @@ number = st.text_input('Nomor Hp. :')
 password = st.text_input('Kata Sandi :')
 
 st.write('Nama Lengkap : ',nama)
-st.write('Umur : ',umur)
 st.write('Nomor Hp  : ',number)
 st.write('Kata Sandi : ',password)
 

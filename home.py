57421412e7ad88a3e50d38f83eb57e48eb4e45af
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-21 14:26:37 +0700

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,8 @@ from PIL import Image
 image = Image.open('kasir.png')
 image1 = Image.open('text-kasir.png')
 
-st.image(image,image1, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(image1, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 
 components.html(

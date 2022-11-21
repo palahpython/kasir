3cb73c78eaae11c60a2c5376109c5b3d9fdf8c55
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-21 14:53:14 +0700

    Update home.py

--- home.py
+++ home.py
@@ -16,14 +16,4 @@ with tab3:
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
    
    
-   st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+  

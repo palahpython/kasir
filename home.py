85d0b5864e70a63917076d7f9af6bb6edef191f8
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-21 14:39:18 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,14 +3,16 @@ import streamlit.components.v1 as components
 from PIL import Image
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+image = Image.open('kasir.png')
+image0 = Image.open('text-kasir.png')
 
 with tab1:
    st.header("A cat")
-   st.image('kasir.png', width=200)
+   st.image('image, width=200)
 
 with tab2:
    st.header("A dog")
-   st.image('text-kasir.png', width=200)
+   st.image('image0', width=200)
 
 with tab3:
    st.header("An owl")

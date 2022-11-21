d21811d746b64b12ce3bee5b6a0bcd86107bf43d
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-21 14:25:25 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,9 +3,9 @@ import streamlit.components.v1 as components
 from PIL import Image
 
 image = Image.open('kasir.png')
-image = Image.open('')
+image1 = Image.open('text-kasir.png')
 
-st.image(image, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(image,image1, caption=None, width=100, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 
 components.html(

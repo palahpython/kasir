3680ba85e1cbfc8eb71e74b329f79a8df9aed3a0
palahpython <118160620+palahpython@users.noreply.github.com>
2022-11-21 14:46:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import streamlit as st
+
+
 st.image('kasir.png')
+components.html(
+    """
+    <table>
+    <tr>
+    <td>HOME</td>
+    <td>|<td>
+    <td>Help</td>
+    </tr>
+    </table>
+    """,
+    height=600,
+)
 
 
 st.balloons()
